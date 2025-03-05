3bec265be2c889c83822cb4c453797a4eec82713
Chhean Brosna <brosna.chhean@sisgroup.sg>
2025-03-05 20:11:16 +0700

    Navigation from onboarding to random user view

--- app/src/main/java/com/example/androidkolinmvvm/views/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/androidkolinmvvm/views/onboarding/OnboardingActivity.kt
@@ -1,21 +1,45 @@
 package com.example.androidkolinmvvm.views.onboarding
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidkolinmvvm.R
+import com.example.androidkolinmvvm.databinding.ActivityOnboardingBinding
+import com.example.androidkolinmvvm.views.random_user.RandomUserActivity
 
 class OnboardingActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityOnboardingBinding
+    private lateinit var getStartedButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding)
+
+        viewBinding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setupUI()
+        setupListeners()
+    }
+
+    private fun setupUI() {
+        getStartedButton = viewBinding.btnGetStarted
+    }
+
+    private fun setupListeners() {
+        getStartedButton.setOnClickListener {
+            val intent: Intent = Intent(this, RandomUserActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/androidkolinmvvm/views/random_user/RandomUserActivity.kt
@@ -0,0 +1,21 @@
+package com.example.androidkolinmvvm.views.random_user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidkolinmvvm.R
+
+class RandomUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_random_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
